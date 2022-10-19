cc0764cbf66a4ed3f2ddc7f417b030d457df6645
Yamamotodayo <ocsyamamotoyuto@gmail.com>
2022-10-19 23:34:56 +0900

    削除ボタン・スペースボタンの関数完成

--- script.js
+++ script.js
@@ -1,28 +1,48 @@
+'use strict'
+import { convert2Dakuon } from './convert2Dakuon.js'
+
 let input = document.querySelectorAll("input")
 let named = document.getElementById("name")
 let names = []
-let nameCount = 0
-// let nnn
+let Count = 0
 
-console.log(input);
+// console.log(input);
 
-// input.forEach(element => {
-//     console.log(element);
-// });
 function typed(e) {
-    names[nameCount] = e.target.value 
-    // nnn = e.target.value
-    // named.innerHTML += nnn
-    // named.innerHTML += e.target.value
-    
+    names[Count] = e.target.value     
     named.innerHTML = names.join("")
 
     console.log(named);
-    nameCount++
+    Count++
     console.log(names);
 }
 
-for (i = 0; i < input.length; i++) {
+// 削除ボタン
+function deleteBtn() {
+    if (Count !== 0) { // この条件式がないと配列が空の時に、削除ボタンを押すと添字がマイナスになる
+        names.pop()
+        named.innerHTML = names.join("")
+        Count--
+        console.log(names);
+    }
+}
+
+// スペースボタン
+function spaceBtn() {
+    names[Count] = "　"
+    named.innerHTML = names.join("")
+    Count++
+    console.log(names);
+}
+
+// 濁点ボタン
+function dakuBtn() {
+    let daku = names[names.length - 1]
+    convert2Dakuon(daku)
+}
+
+
+for (let i = 0; i < input.length; i++) {
     input[i].addEventListener("click", function() {
 
         this.blur(".keyboard-wrapper .keys input:focus ")
@@ -34,7 +54,3 @@ for (i = 0; i < input.length; i++) {
     })
 }
 
-function deleteBtn() {
-    let result = nnn.slice(1,3)
-    named += result
-}
